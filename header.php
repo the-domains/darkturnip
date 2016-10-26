72dc42c4e655f9345bf324572f1d41ee210962e3
Nick <punkblade@users.noreply.github.com>
2016-10-26 15:48:56 +0100

    Create header.php

---
+++ header.php
@@ -0,0 +1,18 @@
+<div class="container">
+    <div class="row">
+        <div class="col-sm-12">
+            <h3 class="text-center page-header visible-xs">
+                Cristina Simcea Photography
+            </h3>
+            <h2 class="text-center page-header visible-sm">
+                Cristina Simcea Photography
+            </h2>
+            <h1 class="text-center page-header visible-md">
+                Cristina Simcea Photography
+            </h1>
+            <h1 class="text-center page-header visible-lg">
+                Cristina Simcea Photography
+            </h1>
+        </div>
+    </div>
+</div>
